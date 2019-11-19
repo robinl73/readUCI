40a4f4ce7d29a303a7a21ddc3d939b3aafe6b8aa
clarajrosenberg <clarajrosenberg@gmail.com>
2019-11-19 15:53:53 -0500

    added preview_names function

---
+++ R/preview_names.R
@@ -0,0 +1,24 @@
+#' @export
+#' @param data An existing dataframe that has been pulled in from the UCI
+#'   database.
+#' @param overwrite Default is TRUE. Indicates whether new, clean column names
+#'   should replace the existing column names.
+#'
+preview_names <- function(data, overwrite = TRUE) {
+  #create vector of column names
+  colnames_data <- colnames(data)
+  #check if data has names
+  if (colnames_data[[1]] == "X1") {
+    stop("Your data does not have variable names.")
+  }
+  #if overwrite is TRUE, replace names with clean names
+  if (overwrite == TRUE) {
+    colnames_clean <- janitor::make_clean_names(colnames_data)
+    colnames(data) <- colnames_clean
+    print(colnames(data))
+  }
+  else {
+    #print existing column names
+    print(colnames_data)
+  }
+}
