23c391564256b467d3d59ae799663952933c977e
clarajrosenberg <clarajrosenberg@gmail.com>
2019-12-04 11:07:35 -0500

    preview_names update with documentation

--- R/preview_names.R
+++ R/preview_names.R
@@ -3,6 +3,7 @@
 #'   database.
 #' @param overwrite Default is TRUE. Indicates whether new, clean column names
 #'   should replace the existing column names.
+#' @param ... Additional arguments to be passed to janitor::clean_names
 #' @title preview_names
 #' @examples 
 #' #show and clean names for iris
@@ -10,21 +11,21 @@
 #' iris_uci <- read_UCI("iris", "iris.data")
 #' iris_uci_names <- preview_names(iris_uci)
 #'}
-preview_names <- function(data, overwrite = TRUE) {
+preview_names <- function(data, overwrite = TRUE, ...) {
   #check if data has names
   if (colnames(data)[[1]] == "X1") {
     stop("Your data does not have variable names.")
   }
   #if overwrite is TRUE, replace names with clean names
   if (overwrite == TRUE) {
-    colnames_clean <- janitor::make_clean_names(colnames(data))
+    colnames_clean <- janitor::make_clean_names(colnames(data), ...)
     colnames(data) <- colnames_clean
     print(colnames(data))
-    return(data)
+    invisible(data)
   }
   else {
     #print existing column names
     print(colnames(data))
-    return(data)
+    invisible(data)
   }
 }
