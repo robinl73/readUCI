cf38a25c5182b041cb5e71904bbe668525518705
Emma Livingston <livingston.emma73@gmail.com>
2019-12-15 12:51:38 -0500

    added row and column tests for read_UCI

--- tests/testthat/test_readUCI.R
+++ tests/testthat/test_readUCI.R
@@ -1,3 +1,5 @@
 test_that("read_UCI works", {
   expect_type(read_UCI("iris", "iris.data"), "list")
+  expect_equal(nrow(read_UCI("iris", "iris.data")), 150)
+  expect_equal(ncol(read_UCI("iris", "iris.data")), 5)
 })
