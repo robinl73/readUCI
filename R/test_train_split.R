6638fb0f6b24af152870aaa821cc9da6f43db068
kphan <kphan@bonny.smith.edu>
2022-03-31 19:27:13 -0400

    fixed typo in function

--- R/test_train_split.R
+++ R/test_train_split.R
@@ -12,7 +12,7 @@ test_train_split <- function(dataset, test_size  ) {
     slice((test_size+1):a)
   
   all_datasets$test_input <- all_datasets$test_set[c(1:(b-1))]
-  all_datasets$test_output <- all_datasets$train_set[b]
+  all_datasets$test_output <- all_datasets$test_set[b]
   
   all_datasets$train_input <- all_datasets$train_set[c(1:(b-1))]
   all_datasets$train_output <- all_datasets$train_set[b]
